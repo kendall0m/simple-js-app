4fc1fb04f8336f0165b35129f68382414458a920
kendall0m <marykmiciotto@gmail.com>
2023-07-07 10:23:23 -0400

    created pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
-alert('Hello world');
+let pokemonList = [
+    {name: 'Charizard', type: ['fire', 'flying'], height: '1.7'},
+    {name: 'Gengar', type: ['ghost', 'poison'], height: '1.5'},
+    {name: 'Clefable', type: 'fairy', height: '1.3'},
+];
 
-let favoriteFood = 'spaghetti';
-document.write(favoriteFood);
+console.log(pokemonList)
